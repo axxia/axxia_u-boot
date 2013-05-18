02f1ba205bfebc1e77f79657fdc0f6d1b197668d
John Jacques <john.jacques@lsi.com>
2013-05-18 18:02:09 -0500

    Added a board specific function to fix up the device tree before Linux boots.  Fixes up cpu-release-addr for each cpu...
    Signed-off-by: John Jacques <john.jacques@lsi.com>

--- board/lsi/axxia-arm/axxia.c
+++ board/lsi/axxia-arm/axxia.c
@@ -44,15 +44,29 @@ board_init(void)
 	return 0;
 }
 
-/**
- * @brief misc_init_r - Configure EVM board specific configurations
- * such as power configurations, ethernet initialization as phase2 of
- * boot sequence
- *
- * @return 0
- */
-int misc_init_r(void)
+/*
+  ------------------------------------------------------------------------------
+  misc_init_r
+*/
+
+int
+misc_init_r(void)
 {
+	unsigned long hold_cpu;
+	int i;
+
+	/*
+	  Wake up core 1.
+	*/
+
+#if 0
+	printf("%s:%d - Waking up core 1.\n", __FILE__, __LINE__);
+	writel(0xab, (SYSCON + 0x1000));
+	hold_cpu = readl(SYSCON + 0x1010);
+	hold_cpu &= ~(1 << 1);
+	writel(hold_cpu, (SYSCON + 0x1010));
+#endif
+
 	return 0;
 }
 
@@ -73,4 +87,44 @@ int
 board_early_init_f(void)
 {
 	gd->ram_size = 0x40000000;
+}
+
+/*
+  ------------------------------------------------------------------------------
+  ft_board_setup
+*/
+
+void
+ft_board_setup(void *blob, bd_t *bd)
+{
+	int i;
+	char cpu_string[40];
+	int node;
+	const char *path;
+	unsigned long release_addr;
+	int rc;
+
+	release_addr = htonl(_spin_table_start_ofs + spin_loop_release_offset);
+	printf("%s:%d - release_addr=0x%08x\n", __FILE__, __LINE__, release_addr);
+
+	for (i = 1; i < 16; ++i) {
+		sprintf(cpu_string, "/cpus/cpu@%d", i);
+		node = fdt_path_offset(blob, cpu_string);
+
+		if (0 > node)
+			continue;
+
+		printf("%s:%d - i=%d node=%d\n", __FILE__, __LINE__, i, node);
+
+		rc = fdt_setprop(blob, node, "cpu-release-addr",
+				 &release_addr, sizeof(unsigned long));
+
+		if (0 != rc) {
+			printf("%s:%d - Error setting property, %d!\n",
+			       __FILE__, __LINE__, rc);
+			continue;
+		}
+	}
+
+	return;
 }
