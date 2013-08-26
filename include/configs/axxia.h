2ad081c5319d9267000683901633b526580255c9
John Jacques <john.jacques@lsi.com>
2013-08-26 17:48:30 -0500

    Disable EIOA Ethernet by default. Signed-off-by: John Jacques <john.jacques@lsi.com>

--- include/configs/axxia.h
+++ include/configs/axxia.h
@@ -349,7 +349,7 @@ int write_parameters(parameters_t *);
 #ifndef CONFIG_SPL_BUILD
 
 #define CONFIG_AXXIA_FEMAC
-#define CONFIG_AXXIA_EIOA
+/*#define CONFIG_AXXIA_EIOA*/
 
 #if defined(CONFIG_AXXIA_FEMAC) || defined(CONFIG_AXXIA_EIOA)
 #define CONFIG_AXXIA_NET
