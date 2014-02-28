f7298b0105a6b427aecc15c77a7b8cd477731551
John Jacques <john.jacques@lsi.com>
2014-02-28 12:35:26 -0600

    board/lsi: Add a Command to Verify/Decrypt Secure Images
    Signed-off-by: John Jacques <john.jacques@lsi.com>

--- common/cmd_lsi_sbb.c
+++ common/cmd_lsi_sbb.c
@@ -1,7 +1,7 @@
 /*
  * common/cmd_lsi_sbb.c
  *
- * (C) Copyright 2011
+ * (C) Copyright 2014
  * LSI, john.jacques@lsi.com
  *
  * See file CREDITS for list of people who contributed to this
@@ -23,9 +23,6 @@
  * MA 02111-1307 USA
  */
 
-/*#define LSI_DEBUG*/
-#define LSI_WARN
-/*#define LSI_LOGIO*/
 #include <config.h>
 
 #ifdef CONFIG_ACP
@@ -53,11 +50,15 @@
 */
   
 int
-do_sbb(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
+do_sbb(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
-	int rc = -1;
 	unsigned long source;
 	unsigned long destination;
+
+	if (0 == strncmp(argv[1], "h", strlen("h"))) {
+		printf("%s", cmdtp->usage);
+		return 0;
+	}
 
 	if (0 == strncmp(argv[1], "v", strlen("v"))) {
 		int safe = 0;
@@ -68,59 +69,16 @@ do_sbb(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 		source = simple_strtoul(argv[2], NULL, 16);
 		destination = simple_strtoul(argv[3], NULL, 16);
 
-		if (0 == sbb_verify_image((void *)source, (void *)destination, safe))
-			rc = 0;
-	} else if (0 == strncmp(argv[1], "e", strlen("e"))) {
-		unsigned long size;
-
-		source = simple_strtoul(argv[2], NULL, 16);
-		destination = simple_strtoul(argv[3], NULL, 16);
-		size = simple_strtoul(argv[4], NULL, 16);
-
-		if (0 == sbb_encrypt_range((void *)source, (void *)destination, size))
-			rc = 0;
-	} else if (0 == strncmp(argv[1], "d", strlen("d"))) {
-		int safe = 0;
-
-		if ((5 == argc) && (0 == strncmp(argv[4], "s", strlen("s"))))
-			safe = 1;
-
-		source = simple_strtoul(argv[2], NULL, 16);
-		destination = simple_strtoul(argv[3], NULL, 16);
-
-		if (0 == sbb_decrypt_range((void *)source, (void *)destination, safe))
-			rc = 0;
+		if (0 ==
+		    sbb_verify_image((void *)source, (void *)destination, safe))
+			return 0;
+		else
+			return -1;
 	}
 
-#if 0
-	if (((0 == strncmp(argv[1], "d", strlen("d"))) ||
-	     (0 == strncmp(argv[1], "v", strlen("v")))) &&
-	    (4 == argc)) {
-		unsigned long source;
-		unsigned long destination;
-		int double_read = 0;
+	printf("%s", cmdtp->usage);
 
-		if (0 == strncmp(argv[1], "d", strlen("d")))
-			double_read = 1;
-
-		source = simple_strtoul(argv[2], NULL, 16);
-		destination = simple_strtoul(argv[3], NULL, 16);
-
-		if (0 != sbb_verify_image((void *)source, (void *)destination,
-					  double_read)) {
-			printf("verify failed!\n");
-		} else {
-			rc = 0;
-		}
-	}
-#endif
-
-	if (0 != rc) {
-		printf("%s", cmdtp->usage);
-	}
-
- do_test_exit:
-	return rc;
+	return -1;
 }
 
 /*
@@ -132,8 +90,6 @@ do_sbb(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 U_BOOT_CMD(sbb, 5, 0, do_sbb,
 	   "sbb command source destination [size (encrrypt only)] [safe]\n",
 	   "     command must be validate|encrypt|decrypt\n" \
-	   "     v,alidate -- Validate Image (safe for double read)\n" \
-	   "     e,ncrypt  -- Encrypt Memory Region\n" \
-	   "     d,ecrypt  -- Decrypt Memory Region (safe for double read)\n");
+	   "     v,alidate -- Validate Image (safe for double read)\n");
 
 #endif /* CONFIG_ACP */
