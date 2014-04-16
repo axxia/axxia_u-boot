588376b25f5e57951da7ebda6c6fb1ed2cb36e49
SangeethaRao <sangeetha.rao@lsi.com>
2014-04-16 16:19:08 -0700

    Updated spl_mtest according to BZ 46544. spl_mtest no longer uses invalid dummy pointer for data-line test. Also, spl_mtest function has been extended to use new new arguments to specify number of iterations and type of test (data or addr or mtest or all)
    Signed-off-by: SangeethaRao <sangeetha.rao@lsi.com>

--- board/lsi/axxia-arm/spl.c
+++ board/lsi/axxia-arm/spl.c
@@ -49,15 +49,22 @@ DECLARE_GLOBAL_DATA_PTR;
 
 #ifdef CONFIG_SPL_MTEST
 
-static void
-spl_mtest(unsigned long *start, unsigned long *end)
+typedef enum {
+	data = 0,
+	addr,
+	mtest,
+	all
+} spl_mtest_t;
+
+int
+spl_mtest(unsigned long *start, unsigned long *end, int total_iterations,
+	spl_mtest_t type)
 {
 	vu_long	*addr;
 	ulong	val;
 	ulong	readback;
 	ulong	errs = 0;
-	int iterations = 1;
-	int iteration_limit;
+	int iterations = 0;
 	vu_long	len;
 	vu_long	offset;
 	vu_long	test_offset;
@@ -65,7 +72,7 @@ spl_mtest(unsigned long *start, unsigned long *end)
 	vu_long	temp;
 	vu_long	anti_pattern;
 	vu_long	num_words;
-	vu_long *dummy = NULL;	/* yes, this is address 0x0, not NULL */
+	vu_long *dummy;
 	int	j;
 
 	static const ulong bitpattern[] = {
@@ -80,236 +87,235 @@ spl_mtest(unsigned long *start, unsigned long *end)
 	};
 
 	pattern = 0;
-	iteration_limit = 0;
 
-	printf ("Testing %08x ... %08x:\n", (uint)start, (uint)end);
+	printf("Testing %08x ... %08x:\n", (uint)start, (uint)end);
 
 	for (;;) {
-		if (ctrlc()) {
-			putc ('\n');
-			return 1;
-		}
 
-		if (iteration_limit && iterations > iteration_limit) {
+		if (iterations > total_iterations) {
 			printf("Tested %d iteration(s) with %lu errors.\n",
-			       iterations-1, errs);
+			       iterations, errs);
 			return errs != 0;
 		}
 
+		iterations++;
 		printf("Iteration: %6d\r", iterations);
 		debug("\n");
-		iterations++;
 
-		/*
-		 * Data line test: write a pattern to the first
-		 * location, write the 1's complement to a 'parking'
-		 * address (changes the state of the data bus so a
-		 * floating bus doen't give a false OK), and then
-		 * read the value back. Note that we read it back
-		 * into a variable because the next time we read it,
-		 * it might be right (been there, tough to explain to
-		 * the quality guys why it prints a failure when the
-		 * "is" and "should be" are obviously the same in the
-		 * error message).
-		 *
-		 * Rather than exhaustively testing, we test some
-		 * patterns by shifting '1' bits through a field of
-		 * '0's and '0' bits through a field of '1's (i.e.
-		 * pattern and ~pattern).
-		 */
-		addr = start;
-		for (j = 0; j < sizeof(bitpattern)/sizeof(bitpattern[0]); j++) {
-			val = bitpattern[j];
-			for(; val != 0; val <<= 1) {
-				*addr  = val;
-				/* clear the test data off of the bus */
-				*dummy  = ~val;
-				readback = *addr;
-				if(readback != val) {
-					printf ("FAILURE (data line): "
-						"expected %08lx, actual %08lx\n",
-						val, readback);
-					errs++;
-					if (ctrlc()) {
-						putc ('\n');
-						return 1;
+		if ((type == data) || (type == all)) {
+
+			/*
+			 * Data line test: write a pattern to the first
+			 * location, write the 1's complement to a 'parking'
+			 * address (changes the state of the data bus so a
+			 * floating bus doen't give a false OK), and then
+			 * read the value back. Note that we read it back
+			 * into a variable because the next time we read it,
+			 * it might be right (been there, tough to explain to
+			 * the quality guys why it prints a failure when the
+			 * "is" and "should be" are obviously the same in the
+			 * error message).
+			 *
+			 * Rather than exhaustively testing, we test some
+			 * patterns by shifting '1' bits through a field of
+			 * '0's and '0' bits through a field of '1's (i.e.
+			 * pattern and ~pattern).
+			 */
+			addr = start;
+			dummy = start + 1;
+			for (j = 0;
+				j < sizeof(bitpattern)/sizeof(bitpattern[0]);
+				j++) {
+				for (val = bitpattern[j]; val != 0; val <<= 1) {
+					*addr  = val;
+					/* clear the test data off of the bus */
+					*dummy  = ~val;
+					readback = *addr;
+					if (readback != val) {
+						printf("FAILURE (data line): " \
+							"expected %08lx, actual %08lx\n",
+							val, readback);
+						errs++;
 					}
-				}
-				*addr  = ~val;
-				*dummy  = val;
-				readback = *addr;
-				if(readback != ~val) {
-					printf ("FAILURE (data line): "
-						"Is %08lx, should be %08lx\n",
-						readback, ~val);
-					errs++;
-					if (ctrlc()) {
-						putc ('\n');
-						return 1;
+					*addr  = ~val;
+					*dummy  = val;
+					readback = *addr;
+					if (readback != ~val) {
+						printf("FAILURE (data line): " \
+							"Is %08lx, should be %08lx\n",
+							readback, ~val);
+						errs++;
 					}
 				}
 			}
 		}
 
-		/*
-		 * Based on code whose Original Author and Copyright
-		 * information follows: Copyright (c) 1998 by Michael
-		 * Barr. This software is placed into the public
-		 * domain and may be used for any purpose. However,
-		 * this notice must not be changed or removed and no
-		 * warranty is either expressed or implied by its
-		 * publication or distribution.
-		 */
+		if ((type == addr) || (type == all)) {
+			/*
+			 * Based on code whose Original Author and Copyright
+			 * information follows: Copyright (c) 1998 by Michael
+			 * Barr. This software is placed into the public
+			 * domain and may be used for any purpose. However,
+			 * this notice must not be changed or removed and no
+			 * warranty is either expressed or implied by its
+			 * publication or distribution.
+			 */
 
-		/*
-		 * Address line test
-		 *
-		 * Description: Test the address bus wiring in a
-		 *              memory region by performing a walking
-		 *              1's test on the relevant bits of the
-		 *              address and checking for aliasing.
-		 *              This test will find single-bit
-		 *              address failures such as stuck -high,
-		 *              stuck-low, and shorted pins. The base
-		 *              address and size of the region are
-		 *              selected by the caller.
-		 *
-		 * Notes:	For best results, the selected base
-		 *              address should have enough LSB 0's to
-		 *              guarantee single address bit changes.
-		 *              For example, to test a 64-Kbyte
-		 *              region, select a base address on a
-		 *              64-Kbyte boundary. Also, select the
-		 *              region size as a power-of-two if at
-		 *              all possible.
-		 *
-		 * Returns:     0 if the test succeeds, 1 if the test fails.
-		 */
-		len = ((ulong)end - (ulong)start)/sizeof(vu_long);
-		pattern = (vu_long) 0xaaaaaaaa;
-		anti_pattern = (vu_long) 0x55555555;
+			/*
+			 * Address line test
+			 *
+			 * Description: Test the address bus wiring in a
+			 *              memory region by performing a walking
+			 *              1's test on the relevant bits of the
+			 *              address and checking for aliasing.
+			 *              This test will find single-bit
+			 *              address failures such as stuck -high,
+			 *              stuck-low, and shorted pins. The base
+			 *              address and size of the region are
+			 *              selected by the caller.
+			 *
+			 * Notes:	For best results, the selected base
+			 *              address should have enough LSB 0's to
+			 *              guarantee single address bit changes.
+			 *              For example, to test a 64-Kbyte
+			 *              region, select a base address on a
+			 *              64-Kbyte boundary. Also, select the
+			 *              region size as a power-of-two if at
+			 *              all possible.
+			 *
+			 * Returns:
+			 * 0 if the test succeeds, 1 if the test fails.
+			 */
+			len = ((ulong)end - (ulong)start)/sizeof(vu_long);
+			pattern = (vu_long) 0xaaaaaaaa;
+			anti_pattern = (vu_long) 0x55555555;
 
-		debug("%s:%d: length = 0x%.8lx\n",
-		      __FUNCTION__, __LINE__,
-		      len);
-		/*
-		 * Write the default pattern at each of the
-		 * power-of-two offsets.
-		 */
-		for (offset = 1; offset < len; offset <<= 1) {
-			start[offset] = pattern;
-		}
+			debug("%s:%d: length = 0x%.8lx\n",
+			      __FUNCTION__, __LINE__,
+			      len);
+			/*
+			 * Write the default pattern at each of the
+			 * power-of-two offsets.
+			 */
+			for (offset = 1; offset < len; offset <<= 1)
+				start[offset] = pattern;
 
-		/*
-		 * Check for address bits stuck high.
-		 */
-		test_offset = 0;
-		start[test_offset] = anti_pattern;
-
-		for (offset = 1; offset < len; offset <<= 1) {
-			temp = start[offset];
-			if (temp != pattern) {
-				printf ("\nFAILURE: Address bit stuck high "
-					"@ 0x%.8lx:"
-					" expected 0x%.8lx, actual 0x%.8lx\n",
-					(ulong)&start[offset], pattern, temp);
-				errs++;
-				if (ctrlc()) {
-					putc ('\n');
-					return 1;
-				}
-			}
-		}
-		start[test_offset] = pattern;
-
-		/*
-		 * Check for addr bits stuck low or shorted.
-		 */
-		for (test_offset = 1; test_offset < len; test_offset <<= 1) {
+			/*
+			 * Check for address bits stuck high.
+			 */
+			test_offset = 0;
 			start[test_offset] = anti_pattern;
 
 			for (offset = 1; offset < len; offset <<= 1) {
 				temp = start[offset];
-				if ((temp != pattern) &&
-				    (offset != test_offset)) {
-					printf ("\nFAILURE: Address bit stuck "
-						"low or shorted @"
-						" 0x%.8lx: expected 0x%.8lx, "
-						"actual 0x%.8lx\n",
-						(ulong)&start[offset], pattern,
-						temp);
+				if (temp != pattern) {
+					printf("\nFAILURE: Address bit stuck" \
+						" high @ 0x%.8lx:" \
+						" expected 0x%.8lx," \
+						" actual 0x%.8lx\n", \
+						(ulong)&start[offset],
+						 pattern, temp);
 					errs++;
-					if (ctrlc()) {
-						putc ('\n');
-						return 1;
-					}
 				}
 			}
 			start[test_offset] = pattern;
-		}
 
-		/*
-		 * Description: Test the integrity of a physical
-		 *		memory device by performing an
-		 *		increment/decrement test over the
-		 *		entire region. In the process every
-		 *		storage bit in the device is tested
-		 *		as a zero and a one. The base address
-		 *		and the size of the region are
-		 *		selected by the caller.
-		 *
-		 * Returns:     0 if the test succeeds, 1 if the test fails.
-		 */
-		num_words = ((ulong)end - (ulong)start)/sizeof(vu_long) + 1;
+			/*
+			 * Check for addr bits stuck low or shorted.
+			 */
+			for (test_offset = 1; test_offset < len;
+				test_offset <<= 1) {
+				start[test_offset] = anti_pattern;
 
-		/*
-		 * Fill memory with a known pattern.
-		 */
-		for (pattern = 1, offset = 0; offset < num_words; pattern++,
-			     offset++) {
-			start[offset] = pattern;
-		}
-
-		/*
-		 * Check each location and invert it for the second pass.
-		 */
-		for (pattern = 1, offset = 0; offset < num_words; pattern++,
-			     offset++) {
-			temp = start[offset];
-			if (temp != pattern) {
-				printf ("\nFAILURE (read/write) @ 0x%.8lx:"
-					" expected 0x%.8lx, actual 0x%.8lx)\n",
-					(ulong)&start[offset], pattern, temp);
-				errs++;
-				if (ctrlc()) {
-					putc ('\n');
-					return 1;
+				for (offset = 1; offset < len; offset <<= 1) {
+					temp = start[offset];
+					if ((temp != pattern) &&
+					    (offset != test_offset)) {
+						printf("\nFAILURE: Address bit"\
+							"stuck low or"
+							" shorted @" \
+							" 0x%.8lx:" \
+							" expected 0x%.8lx, "\
+							"actual 0x%.8lx\n",
+							(ulong)&start[offset],
+							pattern,
+							temp);
+						errs++;
+					}
 				}
+				start[test_offset] = pattern;
+			}
+		}
+
+		if ((type == mtest) || (type == all)) {
+			/*
+			 * Description: Test the integrity of a physical
+			 *	memory device by performing an
+			 *	increment/decrement test over the
+			 *	entire region. In the process every
+			 *	storage bit in the device is tested
+			 *	as a zero and a one. The base address
+			 *	and the size of the region are
+			 *	selected by the caller.
+			 *
+			 * Returns:
+			 *     0 if the test succeeds, 1 if the test fails.
+			 */
+			num_words =
+				((ulong)end - (ulong)start)/sizeof(vu_long) + 1;
+
+			/*
+			 * Fill memory with a known pattern.
+			 */
+			for (pattern = 1, offset = 0;
+				offset < num_words; pattern++,
+				     offset++) {
+				start[offset] = pattern;
 			}
 
-			anti_pattern = ~pattern;
-			start[offset] = anti_pattern;
-		}
-
-		/*
-		 * Check each location for the inverted pattern and zero it.
-		 */
-		for (pattern = 1, offset = 0; offset < num_words; pattern++,
-			     offset++) {
-			anti_pattern = ~pattern;
-			temp = start[offset];
-			if (temp != anti_pattern) {
-				printf ("\nFAILURE (read/write): @ 0x%.8lx:"
-					" expected 0x%.8lx, actual 0x%.8lx)\n",
-					(ulong)&start[offset], anti_pattern,
-					temp);
-				errs++;
-				if (ctrlc()) {
-					putc ('\n');
-					return 1;
+			/*
+			 * Check each location and invert it
+			 * for the second pass.
+			 */
+			for (pattern = 1, offset = 0;
+				offset < num_words; pattern++,
+				     offset++) {
+				temp = start[offset];
+				if (temp != pattern) {
+					printf("\nFAILURE (read/write) " \
+						"@ 0x%.8lx:" \
+						" expected 0x%.8lx," \
+						" actual 0x%.8lx)\n",
+						(ulong)&start[offset],
+						pattern, temp);
+					errs++;
 				}
+
+				anti_pattern = ~pattern;
+				start[offset] = anti_pattern;
 			}
-			start[offset] = 0;
+
+			/*
+			 * Check each location for the inverted pattern
+			 * and zero it.
+			 */
+			for (pattern = 1, offset = 0; offset < num_words;
+				pattern++,
+				     offset++) {
+				anti_pattern = ~pattern;
+				temp = start[offset];
+				if (temp != anti_pattern) {
+					printf("\nFAILURE (read/write):"\
+						" @ 0x%.8lx:"
+						" expected 0x%.8lx, "\
+						"actual 0x%.8lx)\n",\
+						(ulong)&start[offset],
+						anti_pattern,
+						temp);
+					errs++;
+				}
+				start[offset] = 0;
+			}
 		}
 	}
 }
@@ -464,10 +470,16 @@ spl_board_init(void)
 	axxia_display_clocks();
 
 #ifdef CONFIG_SPL_MTEST
-	printf("Running the SPL Memory Test, Ctrl-C to Continue\n");
+	printf("Running the SPL Memory Test\n");
 
-	spl_mtest((unsigned long *)0x40000000,
-		  (unsigned long *)0x7fffffff);
+	if (spl_mtest((unsigned long *)0x40000000,
+		  (unsigned long *)0x7fffffff, 10, all)) {
+		printf("SPL Memory Test FAILED\n");
+	} else {
+		printf("SPL Memory Test SUCCESSFUL\n");
+	}
+		
+	
 #endif
 
 #ifdef SYSCACHE_ONLY_MODE
