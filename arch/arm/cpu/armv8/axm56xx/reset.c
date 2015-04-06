50b9358c9afa5a1443937a67de05c05cf540467b
John Jacques <john.jacques@intel.com>
2015-04-06 08:41:16 -0500

    board/lsi: Update Reset to Work On X9
    Signed-off-by: John Jacques <john.jacques@intel.com>

--- arch/arm/cpu/armv8/axm56xx/reset.c
+++ arch/arm/cpu/armv8/axm56xx/reset.c
@@ -3,21 +3,16 @@
 
 void reset_cpu(ulong ignored)
 {
+	unsigned int reset_ctl;
+
 	/*
 	  Chip Reset
 	*/
-	/* Forcing Gen1 speed before reset such that endpoint
-	 * switches to Gen1 and then if Gen2 speed change is initiated
-	 * from RootComplex, it works
-	 */
-	writel(0x00010041, 0xa0120090); /* PEI0 Gen 1 speed */
-	writel(0x00010000, 0xa012117c);
-	writel(0x00010041, 0xa0130090); /* PEI1 Gen 1 speed */
-	writel(0x00010000, 0xa013117c);
-	writel(0x000000ab, 0x90031000); /* Access Key */
-	writel(0x00000040, 0x90031004); /* Internal Boot, 0xffff0000 Target */
- 	writel(0x80000000, 0x9003180c);	/* Set ResetReadDone */
- 	writel(0x00080802, 0x90031008);	/* Chip Reset */
+
+	writel(0xab, SYSCON + 0x2000);
+	reset_ctl = readl(SYSCON + 0x2008);
+	reset_ctl |= 0x2;
+	writel(reset_ctl, SYSCON + 0x2008);
 
 	printf("Reset failed!\n"); /* Should never get here... */
 }
