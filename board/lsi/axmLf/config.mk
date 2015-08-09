640be934456f1a1c65a4d7bd90bd943a1c30c663
John Jacques <john.jacques@intel.com>
2015-08-09 17:51:35 -0500

    board/lsi: Allow Use of Older Tools
    The XLF Cortex-A53 implementation fixes the errata that
    requires the latest gcc/ld.  So, don't require them.
    
    Signed-off-by: John Jacques <john.jacques@intel.com>

--- board/lsi/axmLf/config.mk
+++ board/lsi/axmLf/config.mk
@@ -1,7 +1,8 @@
 PLATFORM_RELFLAGS += -fno-strict-aliasing -Werror
 
-PLATFORM_LDFLAGS += --fix-cortex-a53-843419 --fix-cortex-a53-835769
-KBUILD_CFLAGS += -mfix-cortex-a53-843419 -mfix-cortex-a53-835769
+# Only available in gcc 5 and the latest (as yet unreleased) binutils.
+#PLATFORM_LDFLAGS += --fix-cortex-a53-843419 --fix-cortex-a53-835769
+#KBUILD_CFLAGS += -mfix-cortex-a53-843419 -mfix-cortex-a53-835769
 
 ifdef CROSS_LIBS
 PLATFORM_LDFLAGS += -L $(CROSS_LIBS)
