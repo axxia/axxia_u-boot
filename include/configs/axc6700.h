4a7eccf8767e9db045f635eeca4fafbf696c9667
John Jacques <john.jacques@intel.com>
2015-10-09 10:14:04 -0500

    board/axxia: Add the Axxia Boards to Kconfig
    Kconfig changes to add the Axxia 5500 and 6700 boards
    to the build system.
    
    Signed-off-by: John Jacques <john.jacques@intel.com>

---
+++ include/configs/axc6700.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2015 Intel
+ *
+ * Configuration settings for the Axxia AXC6700
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#endif /* __CONFIG_H */
