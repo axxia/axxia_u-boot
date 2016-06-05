ad7eb7fcbabb9ab267286a3b53aba5df83275a72
John Jacques <john.jacques@intel.com>
2016-06-04 19:04:45 -0500

    board/axxia: Enable Sysmem Init in Simulation
    Signed-off-by: John Jacques <john.jacques@intel.com>

--- board/axxia/common/axxia_initialize.c
+++ board/axxia/common/axxia_initialize.c
@@ -107,7 +107,6 @@ axxia_initialize(void)
 	*/
 
 #ifndef SYSCACHE_ONLY_MODE
-#if !defined(CONFIG_AXXIA_SIM) /* TODO: Get this working in simulation */
 	if ((0 != (global->flags & PARAMETERS_GLOBAL_SET_SMEM)) &&
 	    (0 == sysmem->ddrRecovery)) {
 		ncr_tracer_enable();
@@ -117,7 +116,6 @@ axxia_initialize(void)
 
 		ncr_tracer_disable();
 	}
-#endif
 #endif
 
 	/*
