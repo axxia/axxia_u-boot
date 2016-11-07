3c47e2862f312c0006e43242a8fa784298a2e3b9
John Jacques <john.jacques@intel.com>
2016-11-07 11:53:58 -0600

    Revert "arch/arm: Remove Watchdog Reset in udelay()"
    This reverts commit 5c11828ff1eb9757e5a4e024f89ee9225550ae8e.

--- lib/time.c
+++ lib/time.c
@@ -22,12 +22,24 @@
  */
 
 #include <common.h>
+#include <watchdog.h>
+
+#ifndef CONFIG_WD_PERIOD
+# define CONFIG_WD_PERIOD	(10 * 1000 * 1000)	/* 10 seconds default*/
+#endif
 
 /* ------------------------------------------------------------------------- */
 
 void udelay(unsigned long usec)
 {
-	__udelay (usec);
+	ulong kv;
+
+	do {
+		WATCHDOG_RESET();
+		kv = usec > CONFIG_WD_PERIOD ? CONFIG_WD_PERIOD : usec;
+		__udelay (kv);
+		usec -= kv;
+	} while(usec);
 }
 
 void mdelay(unsigned long msec)
